1fdcbdd75bd0c3b85e8762986440570b4b8beba7
Ozel Yilmazel <ozelyilmazel@vl965-172-31-136-236.wireless.umass.edu>
2024-06-26 16:12:41 -0400

    Bare minimum

---
+++ src/backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import *
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello/")
+def read_root():
+    return {"Hello": "World"}
