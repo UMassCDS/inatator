207d3bc6b74eba0a3081dfd360ddf60ec689a84a
Ozel Yilmazel <ozelyilmazel@vl965-172-31-136-236.wireless.umass.edu>
2024-07-18 13:06:37 -0400

    added separate .env for local

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-
+import os
 from . import tools
-
 
 app = FastAPI()
 
@@ -17,6 +16,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/echo_env/")
+async def echo_env():
+    return {"url": os.environ.get("DATABASE_URL", "ERROR"), "env": dict(os.environ)}
 
 @app.post("/generate_prediction/")
 async def generate_prediction(request: Request):
