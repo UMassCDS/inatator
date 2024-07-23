565f97392c0bc2c6a068fdbd462cd98ba2878152
Ozel Yilmazel <ozelyilmazel@Ozels-MacBook-Pro.local>
2024-07-23 10:53:24 -0400

    checkout

--- src/frontend/src/components/LoadingStatus.js
+++ src/frontend/src/components/LoadingStatus.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingStatus = ({barStatus}) => {
-
+    console.log('Render loading status');
     return <div className="loading-bar" style={{"backgroundColor": barStatus.color}}><p id="loading-status">{barStatus.loadingStatus}</p></div>
 };
 
